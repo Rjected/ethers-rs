aad1ba676745240efc2ec5dfcb15d11c13715cd1
Dan Cline <6798349+Rjected@users.noreply.github.com>
2022-06-18 12:44:11 -0400

    serde tests

--- ethers-core/src/types/transaction/signed.rs
+++ ethers-core/src/types/transaction/signed.rs
@@ -1,6 +1,6 @@
 use ethabi::ethereum_types::U256;
 use fastrlp::{Encodable, Decodable, length_of_length, Header};
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, Deserializer, Serializer};
 use crate::types::{Signature, TransactionRequest, Eip1559TransactionRequest, Eip2930TransactionRequest};
 use super::eip2718::TypedTransaction;
 
@@ -12,10 +12,9 @@ use super::eip2718::TypedTransaction;
 /// serialized transaction, and cause contract calls and other common actions
 /// to default to using the legacy transaction type.
 #[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
-#[cfg_attr(not(feature = "legacy"), serde(tag = "type"))]
 pub struct SignedTransactionRequest {
-    tx: TypedTransaction,
-    sig: Signature,
+    pub tx: TypedTransaction,
+    pub sig: Signature,
 }
 
 impl SignedTransactionRequest {
@@ -38,7 +37,7 @@ impl SignedTransactionRequest {
         };
 
         // the max value for a single byte to represent itself is 0x7f
-        let max_for_header = U256::from(0x7fu8);
+        let max_for_header = U256::from(fastrlp::EMPTY_STRING_CODE);
         // the number of rlp string headers - each U256 can be either a single byte (and is < 0x7f)
         // or less than 32
         let mut headers_len = 0;
@@ -128,3 +127,39 @@ impl Decodable for SignedTransactionRequest {
 
 // TODO: revise decode impls and comments
 // TODO: tests
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use ethabi::ethereum_types::H256;
+
+    use crate::types::SignedTransactionRequest;
+
+    #[test]
+    fn test_typed_tx_without_access_list() {
+        let tx: SignedTransactionRequest = serde_json::from_str(
+            r#"{
+            "gas": "0x186a0",
+            "maxFeePerGas": "0x77359400",
+            "maxPriorityFeePerGas": "0x77359400",
+            "data": "0x5544",
+            "nonce": "0x2",
+            "to": "0x96216849c49358B10257cb55b28eA603c874b05E",
+            "value": "0x5af3107a4000",
+            "type": "0x2",
+            "chainId": "0x539",
+            "accessList": [],
+            "v": "0x1",
+            "r": "0xc3000cd391f991169ebfd5d3b9e93c89d31a61c998a21b07a11dc6b9d66f8a8e",
+            "s": "0x22cfe8424b2fbd78b16c9911da1be2349027b0a3c40adf4b6459222323773f74"
+        }"#,
+        )
+        .unwrap();
+
+        let expected =
+            H256::from_str("0xa1ea3121940930f7e7b54506d80717f14c5163807951624c36354202a8bffda6")
+                .unwrap();
+        let actual = tx.tx.sighash();
+        assert_eq!(expected, actual);
+    }
+}
